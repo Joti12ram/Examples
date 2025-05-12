18d46b39113de4717c5660821caec196d30171d9
Jotiram <manejotiram1533@gmail.com>
2025-05-12 19:50:28 +0530

    added class Test.java

---
+++ src/Beginner.java
@@ -0,0 +1,12 @@
+
+public class Beginner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=20;
+		int b=30;
+		int sum =a+b;
+		System.out.println(sum);
+	}
+
+}
